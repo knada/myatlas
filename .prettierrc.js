58323edd38fafdef99d1399b45901057e11dff99
Daniel Addae <nanaayimda@gmail.com>
2022-02-25 21:06:43 +0000

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  semi: true,
+  tabWidth: 4,
+};
